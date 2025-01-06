708dce0be0ff2da9d6287c2e15875e0eae55e8e0
randiddelpin <158023014+randiddelpin@users.noreply.github.com>
2025-01-06 10:58:17 +0700

    Add files via upload

--- pages/eskul.vue
+++ pages/eskul.vue
@@ -5,14 +5,16 @@
             <hr />
         </div>
         <div class="row px-5 mx-5">
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""> BASKET </div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""> BULUTANGKIS</div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""> BASKET </div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""> BULUTANGKIS</div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""> BASKET </div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""> BULUTANGKIS</div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""> BASKET </div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""> BULUTANGKIS</div>          
+            <div class="col-2 text-center"><img src="/asset/bx.jpg" alt="">BOXING </div>
+            <div class="col-2 text-center pe-3"><img src="/asset/pks.jpg" alt=""> PKS </div>
+            <div class="col-2 text-center"><img src="/asset/pmr.jpg" alt=""> PMR </div>
+            <div class="col-2 text-center pe-3"><img src="/asset/pd.jpg" alt=""> PERISAI DIRI</div>
+            <div class="col-2 text-center"><img src="/asset/r.jpg" alt="">  ROHIS</div>
+            <div class="col-2 text-center pe-3"><img src="/asset/v.jpg" alt="">VOLLY </div>
+            <div class="col-2 text-center"><img src="/asset/basket.webp" alt=""> BASKET </div>
+            <div class="col-2 text-center pe-3"><img src="/asset/sb.jpg" alt=""> SEPAK BOLA</div>   
+            <div class="col-2 text-center pe-3"><img src="/asset/plh.jpg" alt=""> PLH</div>  
+            <div class="col-2 text-center pe-3"><img src="/asset/bt.jpg" alt=""> BULUTANGKIS</div>      
         </div>
     </div>
   </template>
--- pages/galeri.vue
+++ pages/galeri.vue
@@ -5,14 +5,14 @@
             <hr />
         </div>
         <div class="row px-5 mx-5">
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""></div>
-            <div class="col-2 text-center"><img src="/asset/logo.png" alt=""> </div>
-            <div class="col-2 text-center pe-3"><img src="/asset/logo.png" alt=""></div>          
+            <div class="col-2 text-center"><img src="/asset/g1.jpg" alt=""></div>
+            <div class="col-2 text-center pe-3"><img src="/asset/g2.jpg" alt=""></div>
+            <div class="col-2 text-center"><img src="/asset/g3.jpg" alt=""></div>
+            <div class="col-2 text-center pe-3"><img src="/asset/j4.jpeg" alt=""></div>
+            <div class="col-2 text-center"><img src="/asset/j5.jpeg" alt=""></div>
+            <div class="col-2 text-center pe-3"><img src="/asset/j6.jpeg" alt=""></div>
+            <div class="col-2 text-center"><img src="/asset/j6.jpeg" alt=""> </div>
+            <div class="col-2 text-center pe-3"><img src="/asset/p1 (1).jpeg" alt=""></div>          
         </div>
     </div>
   </template>
